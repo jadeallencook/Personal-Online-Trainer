98207c241cc19b5ef7c3904885d3487b7fec08a8
Jade Allen Cook <jadeallencook@gmail.com>
2018-12-27 19:12:47 -0700

    🛠 Exported production build

---
+++ build/precache-manifest.cf9c44c1a05465d52a3998c4b75abf09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3be31ba44165fa83064c",
+    "url": "/static/js/main.3be31ba4.chunk.js"
+  },
+  {
+    "revision": "e15fdbb50c4fa78b3830",
+    "url": "/static/js/1.e15fdbb5.chunk.js"
+  },
+  {
+    "revision": "3be31ba44165fa83064c",
+    "url": "/static/css/main.547393a4.chunk.css"
+  },
+  {
+    "revision": "e15fdbb50c4fa78b3830",
+    "url": "/static/css/1.971083ba.chunk.css"
+  },
+  {
+    "revision": "cb92add455445c7c8589d70972ef2a67",
+    "url": "/index.html"
+  }
+];
